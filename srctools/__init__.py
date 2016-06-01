6696cf1dbf3579e66c0539495fb4fb5caabbf41c
TeamSpen210 <spencerb21@live.com>
2016-06-01 14:46:08 +1000

    Move BSP to srctools, as well as AtomicWriter

--- srctools/__init__.py
+++ srctools/__init__.py
@@ -1,4 +1,6 @@
+import os as _os
 import string as _string
+import tempfile as _tempfile
 from collections import abc as _abc
 
 from typing import Union as _Union
@@ -13,7 +15,7 @@ __all__ = [
     'clean_line', 'is_plain_text', 'blacklist', 'whitelist',
     'bool_as_int', 'conv_bool', 'conv_int', 'conv_float',
 
-    'EmptyMapping',
+    'EmptyMapping', 'AtomicWriter',
 ]
 
 _FILE_CHARS = set(_string.ascii_letters + _string.digits + '-_ .|')
@@ -181,6 +183,64 @@ class EmptyMapping(_abc.MutableMapping):
         raise KeyError
 
 EmptyMapping = EmptyMapping()  # We only want the one instance
+
+
+class AtomicWriter:
+    """Atomically overwrite a file.
+
+    Use as a context manager - the returned temporary file
+    should be written to. When cleanly exiting, the file will be transfered.
+    If an exception occurs in the body, the temporary data will be discarded.
+
+    This is not reentrant, but can be repeated - starting the context manager
+    clears the file.
+    """
+    def __init__(self, filename, is_bytes=False):
+        """Create an AtomicWriter.
+        is_bytes sets text or bytes writing mode. The file is always writable.
+        """
+        self.filename = filename
+        self.dir = _os.path.dirname(filename)
+        self.is_bytes = is_bytes
+        self.temp = None
+
+    def make_tempfile(self):
+        """Create the temporary file object."""
+        if self.temp is not None:
+            # Already open - close and delete the current file.
+            self.temp.close()
+            _os.remove(self.temp.name)
+
+        # Create folders if needed..
+        _os.makedirs(self.dir, exist_ok=True)
+
+        self.temp = _tempfile.NamedTemporaryFile(
+            mode='wb' if self.is_bytes else 'wt',
+            dir=self.dir,
+            delete=False,
+        )
+
+    def __enter__(self):
+        """Delagate to the underlying temporary file handler."""
+        self.make_tempfile()
+        return self.temp.__enter__()
+
+    def __exit__(self, exc_type, exc_value, tback):
+        # Pass to tempfile, which also closes().
+        temp_path = self.temp.name
+        self.temp.__exit__(exc_type, exc_value, tback)
+        self.temp = None
+        if exc_type is not None:
+            # An exception occured, clean up.
+            try:
+                _os.remove(temp_path)
+            except FileNotFoundError:
+                pass
+        else:
+            # No exception, commit changes
+            _os.replace(temp_path, self.filename)
+
+        return False  # Don't cancel the exception.
 
 
 # Import these, so people can reference 'srctools.Vec' instead of
