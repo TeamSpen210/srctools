a8ba443de2fb2fb66fae6afcc5dd5a4048f7a04a
TeamSpen210 <spencerb21@live.com>
2017-01-11 16:09:00 +1000

    Add setup.py script

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='srctools',
+    version='1.0.3',
+    description="Modules for working with Valve's Source Engine file formats.",
+    url='https://github.com/TeamSpen210/srctools',
+
+    author='TeamSpen210',
+    author_email='spencerb21@live.com',
+    license='unlicense',
+
+    keywords='',
+    classifiers=[
+        'License :: Public Domain',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    packages=['srctools'],
+    install_requires=[
+        # Only needed in 3.4, it's in stdlib
+        # for future versions.
+        'typing; python_version<"3.5"',
+    ],
+)
