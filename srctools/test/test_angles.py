55e376d2bd0e6d27bc3220aa464f5a8afa909082
TeamSpen210 <TeamSpen210@users.noreply.github.com>
2017-03-17 13:59:12 +1000

    Rename test_rotation to test_angles

--- srctools/test/test_angles.py
+++ srctools/test/test_angles.py
@@ -1,4 +1,4 @@
-from srctools import Vec, Angle, RotationMatrix
+from srctools import Vec, Angle
 from srctools.test.test_vec import assert_vec
 import pytest
 
@@ -28,20 +28,3 @@ def assert_ang(ang: Angle, pitch, yaw, roll, msg=''):
         new_msg += ': ' + str(msg)
     pytest.fail(new_msg)
 
-
-def test_matrix_conversion():
-    values = [0, -90, 90, 270, 37, 28]
-    for p in values:
-        for y in values:
-            for r in values:
-                ang = Angle(p, y, r)
-                mat = RotationMatrix()
-                mat.rotate_by_angle(ang)
-
-                v = Vec(50)
-                v2 = v * mat
-                v.rotate(p, y, r)
-                assert_vec(v, v2.x, v2.y, v2.z, (ang, mat))
-
-                new_ang = mat.to_angle()
-                assert_ang(new_ang, p, y, r)
