c084dc7ff106435b658478b28a5135ec735e426e
TeamSpen210 <spencerb21@live.com>
2017-03-30 12:24:09 +1000

    Copy VRAD code from BEE2
    This transparently passes to the original.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    name='VBSP_VRAD',
+    options={
+        'build_exe': {
+            'build_exe': 'build/',
+            # Include all modules in the zip..
+            'zip_include_packages': '*',
+            'zip_exclude_packages': '',
+        },
+    },
+    executables=[
+        # Executable(
+        #     'seecompiler/vbsp.py',
+        #     base='Console',
+        #     targetName='vbsp.exe',
+        # ),
+        Executable(
+            'seecompiler/vrad.py',
+            base='Console',
+            targetName='vrad.exe',
+        ),
+    ]
+)
+
