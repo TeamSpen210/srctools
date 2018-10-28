24e896e4d4e39b91df915a1ae7f6bd9922f1d9a0
TeamSpen210 <spencerb21@live.com>
2018-10-28 10:23:03 +1000

    Make building postcompiler possible

--- build_compiler.py
+++ build_compiler.py
@@ -1,30 +1,45 @@
 from cx_Freeze import setup, Executable
 
+import srctools
+import os
+
+zip_includes = [
+    # Add the FGD data for us.
+    (os.path.join(srctools.__path__[0], 'fgd.lzma'), 'srctools/fgd.lzma'),
+]
+
+# We need to include this version data.
+try:
+    import importlib_resources
+    zip_includes.append(
+        (
+            os.path.join(importlib_resources.__path__[0], 'version.txt'),
+            'importlib_resources/version.txt',
+         )
+    )
+except ImportError:
+    pass
+
+
 setup(
-    name='VBSP_VRAD',
+    name='srctools PostCompiler',
+    version='1.0',
     options={
         'build_exe': {
             'build_exe': 'build/',
             # Include all modules in the zip..
             'zip_include_packages': '*',
             'zip_exclude_packages': '',
-            'zip_includes': [
-                ('srctools/fgd.lzma', 'srctools/fgd.lzma'),
-                ('srctools/srctools.fgd', 'srctools.fgd'),
-            ],
+            'zip_includes': zip_includes,
+            'excludes': ['tkinter', 'PIL'],
         },
     },
     requires=['setuptools', 'cx_freeze'],
     executables=[
-        # Executable(
-        #     'srctools/scripts/vbsp.py',
-        #     base='Console',
-        #     targetName='vbsp.exe',
-        # ),
         Executable(
-            'srctools/scripts/vrad.py',
+            'srctools/scripts/postcompiler.py',
             base='Console',
-            targetName='vrad.exe',
+            targetName='postcompiler.exe',
         ),
     ]
 )
