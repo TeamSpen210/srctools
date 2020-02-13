82e41556edbcaf7c79a47d575c22dac5af5a00fb
TeamSpen210 <spencerb21@live.com>
2020-02-13 12:04:09 +1000

    Add a whole pile of additional ents to class resources

--- srctools/_class_resources.py
+++ srctools/_class_resources.py
@@ -38,17 +38,266 @@ def cls_func(func: ClassFunc) -> ClassFunc:
     CLASS_RESOURCES[func.__name__] = func
     return func
 
+def mdl(path: str) -> Tuple[str, FileType]:
+    """Convienence function."""
+    return (path, FileType.MODEL)
+
+def mat(path: str) -> Tuple[str, FileType]:
+    """Convienence function."""
+    return (path, FileType.MATERIAL)
+
+def sound(path: str) -> Tuple[str, FileType]:
+    """Convienence function."""
+    return (path, FileType.GAME_SOUND)
+
+def part(path: str) -> Tuple[str, FileType]:
+    """Convienence function."""
+    return (path, FileType.PARTICLE)
+
 # In alphabetical order: 
 
-res('env_fire_trail',
-    'materials/sprites/flamelet1.vmt',
-    'materials/sprites/flamelet2.vmt',
-    'materials/sprites/flamelet3.vmt',
-    'materials/sprites/flamelet4.vmt',
-    'materials/sprites/flamelet5.vmt',
-    'materials/particle/particle_smokegrenade.vmt',
-    'materials/particle/particle_noisesphere.vmt',
+res('ai_ally_manager')
+res('ai_battle_line')
+res('ai_changehintgroup')
+res('ai_changetarget')
+res('ai_citizen_response_system')
+res('ai_goal_actbusy')
+res('ai_goal_actbusy_queue')
+res('ai_goal_assault')
+res('ai_goal_fear')
+res('ai_goal_fightfromcover')
+res('ai_goal_follow')
+res('ai_goal_injured_follow')
+res('ai_goal_lead')
+res('ai_goal_lead_weapon')
+res('ai_goal_operator')
+res('ai_goal_police')
+res('ai_goal_standoff')
+res('ai_npc_eventresponsesystem')
+res('ai_relationship')
+res('ai_relationship_classify')
+res('ai_script_conditions')
+res('ai_sound')
+res('ai_speechfilter')
+res('ai_weaponmodifier')
+
+res('ambient_generic')  # Sound is a keyvalue
+res('ambient_music')
+
+res('assault_assaultpoint')
+res('assault_rallypoint')
+
+res('asw_alien_goo',
+    mdl('models/aliens/biomass/biomasshelix.mdl'),
+    mdl('models/aliens/biomass/biomassl.mdl'),
+    mdl('models/aliens/biomass/biomasss.mdl'),
+    mdl('models/aliens/biomass/biomassu.mdl'),
+    sound('ASWGoo.GooLoop'),
+    sound('ASWGoo.GooScream'),
+    sound('ASWGoo.GooDissolve'),
+    sound('ASWFire.AcidBurn'),
+    part('biomass_dissolve'),
+    part('acid_touch'),
+    part('grubsack_death'),
+    includes='asw_grub',
     )
+CLASS_RESOURCES['asw_ambient_generic'] = CLASS_RESOURCES['ambient_generic']  # Identical.
+res('asw_ammo_autogun', mdl('models/swarm/ammo/ammoautogun.mdl'))
+
+res('asw_ammo_drop', 
+    mdl("models/items/Ammobag/AmmoBag.mdl" ),
+    sound("ASW_Ammobag.DropImpact"),
+    sound("ASW_Ammobag.Pickup_sml"),
+    sound("ASW_Ammobag.Pickup_med"),
+    sound("ASW_Ammobag.Pickup_lrg"),
+    sound("ASW_Ammobag.Fail" ),
+    part("ammo_satchel_take_sml"),
+    part("ammo_satchel_take_med"),
+    part("ammo_satchel_take_lrg"),
+    )
+
+
+res('asw_ammo_flamer', mdl('models/swarm/ammo/ammoflamer.mdl'))
+res('asw_ammo_mining_laser', mdl('models/swarm/ammo/ammobattery.mdl'))
+res('asw_ammo_pdw', mdl('models/swarm/ammo/ammopdw.mdl'))
+res('asw_ammo_pistol', mdl('models/swarm/ammo/ammopistol.mdl'))
+res('asw_ammo_railgun', mdl('models/swarm/ammo/ammorailgun.mdl'))  # Commented out
+res('asw_ammo_rifle', mdl('models/swarm/ammo/ammoassaultrifle.mdl'))
+res('asw_ammo_shotgun', mdl('models/swarm/ammo/ammoshotgun.mdl'))
+res('asw_ammo_vindicator', mdl('models/swarm/ammo/ammovindicator.mdl'))
+
+res('asw_barrel_explosive',
+    mdl('models/swarm/Barrel/barrel.mdl'),
+    part('explosion_barrel'),
+    sound('ASWBarrel.Explode'),
+    )
+
+res('asw_barrel_radioactive',
+    mdl('models/swarm/Barrel/barrel.mdl'),
+    part('barrel_rad_gas_cloud'),
+    part('barrel_rad_gas_jet'),
+    sound('Misc.Geiger'),
+    )
+
+res('asw_bloodhound', mdl('models/swarmprops/Vehicles/BloodhoundMesh.mdl'))
+
+res('asw_boomer',
+    part("boomer_explode"),
+    part("joint_goo" ),
+    mdl("models/aliens/boomer/boomerLegA.mdl"),
+    mdl("models/aliens/boomer/boomerLegB.mdl"),
+    mdl("models/aliens/boomer/boomerLegC.mdl"),
+    mdl('models/aliens/boomer/boomer.mdl'),
+    sound("ASW_Boomer.Death_Explode"),
+    sound("ASW_Boomer.Death_Gib"),
+    )
+
+res('asw_broadcast_camera')
+res('asw_buzzer',
+    mdl('models/aliens/buzzer/buzzer.mdl'),
+    sound("ASW_Buzzer.Attack"),
+    sound("ASW_Buzzer.Death"),
+    sound("ASW_Buzzer.Pain"),
+    sound("ASW_Buzzer.Idle"),
+    sound("ASW_Buzzer.OnFire"),
+
+    part("buzzer_trail" ),
+    part("buzzer_death" ),
+    sound("ASWFire.BurningFlesh"),
+    sound("ASWFire.StopBurning"),
+    )
+res('asw_client_corpse')
+
+res('asw_colonist',
+    mdl("models/swarm/Colonist/Male/MaleColonist.mdl"),
+    sound("NPC_Citizen.FootstepLeft"),
+    sound("NPC_Citizen.FootstepRight"),
+    sound("NPC_Citizen.Die"),
+    sound("MaleMarine.Pain"),
+    )
+
+res('asw_debrief_info')
+res('asw_director_control')
+
+res('asw_door',
+    mdl('models/swarm/doors/swarm_singledoor.mdl'),
+    mdl('models/swarm/doors/swarm_singledoor_flipped.mdl'),
+    mdl('models/props/doors/heavy_doors/doorleft.mdl'),
+    mdl('models/props/doors/heavy_doors/doorright.mdl'),
+    sound('ASW_Door.Dented'),
+    sound('ASW_Door.MeleeHit'),
+    sound('ASW_Welder.WeldDeny'),
+    )
+
+res('asw_drone',
+    sound('ASW_Drone.Land'),
+    sound('ASW_Drone.Pain'),
+    sound('ASW_Drone.Alert'),
+    sound('ASW_Drone.Death'),
+    sound('ASW_Drone.Attack'),
+    sound('ASW_Drone.Swipe'),
+
+    sound('ASW_Drone.GibSplatHeavy'),
+    sound('ASW_Drone.GibSplat'),
+    sound('ASW_Drone.GibSplatQuiet'),
+    sound('ASW_Drone.DeathFireSizzle'),
+
+    mdl('models/aliens/drone/ragdoll_tail.mdl'),
+    mdl('models/aliens/drone/ragdoll_uparm.mdl'),
+    mdl('models/aliens/drone/ragdoll_uparm_r.mdl'),
+    mdl('models/aliens/drone/ragdoll_leg_r.mdl'),
+    mdl('models/aliens/drone/ragdoll_leg.mdl'),
+    mdl('models/aliens/drone/gib_torso.mdl'),
+    mdl('models/aliens/drone/drone.mdl'),
+    )
+CLASS_RESOURCES['asw_drone_jumper'] = CLASS_RESOURCES['asw_drone']  # Identical
+res('asw_drone_uber', includes='asw_drone')  # Seems the same resources.
+
+res('asw_egg',
+    part('egg_open'),
+    part('egg_hatch'),
+    part('egg_death'),
+
+    mdl('models/aliens/egg/egggib_1.mdl'),
+    mdl('models/aliens/egg/egggib_2.mdl'),
+    mdl('models/aliens/egg/egggib_3.mdl'),
+    mdl('models/aliens/egg/egg.mdl'),
+
+    sound('ASW_Egg.Open'),
+    sound('ASW_Egg.Gib'),
+    sound('ASW_Parasite.EggBurst'),
+    includes='asw_parasite',
+    )
+
+@cls_func
+def asw_emitter(ent: Entity):
+    """Complicated thing, probably can't fully process here."""
+    template = ent['template']
+    if template and template != 'None':
+        yield 'resource/particletemplates/{}.ptm'.format(template)
+
+    # TODO: Read the following keys from the file:
+    # - "material"
+    # - "glowmaterial"
+    # - "collisionsound"
+    # - "collisiondecal"
+
+    yield mat('materials/effects/yellowflare.vmt')
+
+@cls_func
+def asw_env_explosion(ent: Entity):
+    """Handle the no-sound spawnflag."""
+    yield part("asw_env_explosion")
+    if (conv_int(ent['spawnflags']) & 0x00000010) == 0:
+        yield sound('ASW_Explosion.Explosion_Default')
+
+res('asw_parasite',
+    mdl('models/aliens/parasite/parasite.mdl'),
+    sound('ASW_Parasite.Death'),
+    sound('ASW_Parasite.Attack'),
+    sound('ASW_Parasite.Idle'),
+    sound('ASW_Parasite.Pain'),
+    sound('ASW_Parasite.Attack'),
+    )
+
+CLASS_RESOURCES['asw_parasite_defanged'] = CLASS_RESOURCES['asw_parasite']  # Identical.
+
+res('env_alyxemp',
+    mat('materials/effects/laser1.vmt'),
+    sound('AlyxEmp.Charge'),
+    sound('AlyxEmp.Discharge'),
+    sound('AlyxEmp.Stop'),
+    )
+res('env_ar2explosion')
+
+res('env_beam')
+res('env_beverage', mdl('models/can.mdl'))
+res('env_blood')
+res('env_bubbles', mat('materials/sprites/bubble.vmt'))
+
+
+@cls_func
+def env_break_shooter(ent: Entity):
+    """Special behaviour on the 'model' KV."""
+    if conv_int(ent['modeltype']) == 1: # MODELTYPE_MODEL
+        yield mdl(ent['model'])
+    # Otherwise, a template name or a regular gib.
+
+
+res('env_fire_trail',
+    mat('materials/sprites/flamelet1.vmt'),
+    mat('materials/sprites/flamelet2.vmt'),
+    mat('materials/sprites/flamelet3.vmt'),
+    mat('materials/sprites/flamelet4.vmt'),
+    mat('materials/sprites/flamelet5.vmt'),
+    mat('materials/particle/particle_smokegrenade.vmt'),
+    mat('materials/particle/particle_noisesphere.vmt'),
+    )
+res('env_funnel', mat('materials/sprites/flare6.vmt'))
+
+res('env_global')
+res('env_global_light')
+res('env_gunfire')
 
 res('env_screeneffect',
     'materials/effects/stun.vmt',
