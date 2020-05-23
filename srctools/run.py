60d91db464821b24df62b8fe9e175f2cdfbdc8a4
TeamSpen210 <spencerb21@live.com>
2020-05-23 11:04:54 +1000

    Fix compiler output blocking on streams

--- srctools/run.py
+++ srctools/run.py
@@ -5,8 +5,11 @@ import os.path
 import sys
 import subprocess
 import logging
+import threading
 
 from srctools.logger import get_logger
+
+__all__ = ['run_compiler', 'get_compiler_name']
 
 
 def quote(txt: str) -> str:
@@ -39,11 +42,9 @@ def run_compiler(
     The provided logger will be given the output from the compiler.
     The process exit code is returned.
     """
-    logger.info("Calling original {}...", name.upper())
+    log_name = os.path.basename(name)
+    logger.info("Calling original {}...", log_name.upper())
     logger.info('Args: {}', ', '.join(map(repr, args)))
-
-    buf_out = bytearray()
-    buf_err = bytearray()
 
     comp_name = get_compiler_name(name) if change_name else name
 
@@ -66,48 +67,78 @@ def run_compiler(
         startupinfo=startup_info,
         cwd=os.path.dirname(comp_name) or None,
     ) as proc:
-        # Loop reading data from the subprocess, until it's dead.
-        stdout = proc.stdout  # type: IO[bytes]
-        stderr = proc.stderr  # type: IO[bytes]
-        while proc.poll() is None:  # Loop until dead.
-            buf_out.extend(stdout.read())
-            buf_err.extend(stderr.read())
-
-            _pop_lines(logger, logging.ERROR, name, buf_err)
-            _pop_lines(logger, logging.INFO, name, buf_out)
-
-        # Grab any extra lines still in the pipe.
-        buf_out.extend(stdout.read())
-        buf_err.extend(stderr.read())
-        _pop_lines(logger, logging.ERROR, name, buf_err)
-        _pop_lines(logger, logging.INFO, name, buf_out)
+        # Run both threads, then wait for it to die.
+        thread_out = threading.Thread(
+            target=_daemon,
+            args=(logger, logging.INFO, log_name, proc.stdout),
+        )
+        thread_err = threading.Thread(
+            target=_daemon,
+            args=(logger, logging.ERROR, log_name,  proc.stderr),
+        )
+        thread_out.daemon = thread_err.daemon = True  # These should be killed on shutdown.
+        thread_err.start()
+        thread_out.start()
+        proc.wait()
+    # Exiting the with: causes the streams to be closed, so these should terminate.
+    thread_out.join()
+    thread_err.join()
 
     return proc.returncode
 
 
-def _pop_lines(logger: logging.Logger, loglevel: int, compname: str, buf: bytearray) -> None:
-    """Pull off as many complete lines as possible, logging them."""
-    if b'\r' in buf:
-        buf[:] = buf.replace(b'\r\n', b'\n').replace(b'\r', b'\n')
-    while True:
-        try:
-            newline_off = buf.index(b'\n')
-        except ValueError:
-            return
-        else:
-            # Discard any invalid ASCII - we don't really care.
-            line = buf[:newline_off].decode('ascii', 'ignore')
-            del buf[:newline_off + 1]
+def _daemon(logger: logging.Logger, loglevel: int, compname: str, stream: IO[bytes]) -> None:
+    """Handles logging each stream.
 
-            # Generate a logging record directly, so the logs appear to "come"
-            # from the compiler itself.
-            logger.handle(logger.makeRecord(
-                "subproc",
-                loglevel,
-                "<valve>",
-                0,  # Line number.
-                line,
-                (),
-                None,
-                compname,
-            ))
+    Continuously pull off data, then log whenever they're complete.
+    """
+    buf = bytearray()
+    while not stream.closed:
+        try:
+            buf.extend(stream.read(1))
+        except ValueError:
+            # Closed file.
+            break
+        # If it's \r, we need to see if the next is \n so we can skip.
+        if buf.endswith(b'\r'):
+            continue
+
+        if b'\r' in buf:
+            buf[:] = buf.replace(b'\r\n', b'\n').replace(b'\r', b'\n')
+        while True:
+            try:
+                newline_off = buf.index(b'\n')
+            except ValueError:
+                break
+            else:
+                # Discard any invalid ASCII - we don't really care.
+                line = buf[:newline_off].decode('ascii', 'ignore')
+                del buf[:newline_off + 1]
+
+                # Generate a logging record directly, so the logs appear to
+                # "come"
+                # from the compiler itself.
+                logger.handle(logger.makeRecord(
+                    "subproc",
+                    loglevel,
+                    "<valve>",
+                    0,  # Line number.
+                    line,
+                    (),
+                    None,
+                    compname,
+                ))
+    if buf:
+        # There's still data we haven't emitted, so just dump it all
+        # out at the end.
+        buf[:] = buf.replace(b'\r\n', b'\n').replace(b'\r', b'\n')
+        logger.handle(logger.makeRecord(
+            "subproc",
+            loglevel,
+            "<valve>",
+            0,  # Line number.
+            buf.decode('ascii', 'ignore'),
+            (),
+            None,
+            compname,
+        ))
