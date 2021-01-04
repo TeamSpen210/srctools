94ae058b67aecae416973eeb2a7d7006503a2aed
TeamSpen210 <spencerb21@live.com>
2021-01-04 12:59:38 +1000

    Add warning for imports of old module names

---
+++ srctools/vec.py
@@ -0,0 +1,9 @@
+"""Deprecated, this was renamed to math."""
+import warnings
+warnings.warn(
+    'srctools.vec module was deprecated, import srctools.math instead.',
+    DeprecationWarning,
+    stacklevel=2,
+)
+del warnings
+from srctools.math import *
