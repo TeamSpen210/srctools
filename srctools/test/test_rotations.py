b029922670aef3d8a8db205582bacff1899e7fd7
Spencer Brown <spencerb21@live.com>
2021-06-30 12:43:29 +1000

    Make rotation data a fixture, so the individual tests can be split up

--- srctools/test/test_rotations.py
+++ srctools/test/test_rotations.py
@@ -2,9 +2,53 @@
 import copy
 import os.path
 import pickle
+from typing import NamedTuple, List, Tuple
 
 from srctools.test import *
-from srctools import Vec, Matrix, Angle
+from srctools import Vec
+
+
+class RotationData(NamedTuple):
+    """Result of the rotation_data fixture."""
+    angle: Tuple[float, float, float]
+
+    for_x: float
+    for_y: float
+    for_z: float
+
+    left_x: float
+    left_y: float
+    left_z: float
+
+    up_x: float
+    up_y: float
+    up_z: float
+
+
+@pytest.fixture(scope='session')
+def rotation_data() -> List[RotationData]:
+    """Parse the rotation data dumped from the engine, used to check our math."""
+    data = []
+    with open(os.path.join(RESLOC, 'rotation.txt')) as f:
+        for line_num, line in enumerate(f, start=1):
+            if not line.startswith('|'):
+                # Skip other junk in the log.
+                continue
+            (
+                pit, yaw, roll,
+                for_x, for_y, for_z,
+                right_x, right_y, right_z,
+                up_x, up_y, up_z
+            ) = map(float, line[1:].split())
+            # The engine actually gave us a right vector, so we need to flip that.
+            left_x, left_y, left_z = -right_x, -right_y, -right_z
+            data.append(RotationData(
+                (pit, yaw, roll),
+                for_x, for_y, for_z,
+                left_x, left_y, left_z,
+                up_x, up_y, up_z
+            ))
+    return data
 
 
 def test_vec_identities(py_c_vec: PyCVec) -> None:
@@ -80,7 +124,7 @@ def test_ang_matrix_roundtrip(py_c_vec: PyCVec) -> None:
     Vec, Angle, Matrix, parse_vec_str = py_c_vec
 
     for p, y, r in iter_vec(range(0, 360, 90)):
-        vert = Vec(x=1).rotate(p, y, r).z
+        vert = (Vec(x=1) @ Angle(p, y, r)).z
         if vert < 0.99 or vert > 0.99:
             # If nearly vertical, gimbal lock prevents roundtrips.
             continue
@@ -97,7 +141,7 @@ def test_to_angle_roundtrip(py_c_vec: PyCVec) -> None:
             continue
         norm = Vec(x, y, z).norm()
         ang = norm.to_angle()
-        assert_vec(Vec(x=1).rotate(*ang), norm.x, norm.y, norm.z, ang)
+        assert_vec(Vec(x=1) @ ang, norm.x, norm.y, norm.z, ang)
 
 
 def test_matrix_roundtrip_pitch(py_c_vec: PyCVec) -> None:
@@ -275,80 +319,81 @@ def test_bad_from_basis(py_c_vec: PyCVec) -> None:
         Matrix.from_basis(z=v)
 
 
-def test_gen_check(py_c_vec: PyCVec) -> None:
-    """Do an exhaustive check on all rotation math using data from the engine."""
+def test_rotation_w_engine_data(py_c_vec: PyCVec, rotation_data) -> None:
+    """Test our rotation code with engine rotation data."""
     Vec, Angle, Matrix, parse_vec_str = py_c_vec
 
     X = Vec(x=1)
     Y = Vec(y=1)
     Z = Vec(z=1)
-    with open(os.path.join(RESLOC, 'rotation.txt')) as f:
-        for line_num, line in enumerate(f, start=1):
-            if not line.startswith('|'):
-                # Skip other junk in the log.
-                continue
 
-            (
-                pit, yaw, roll,
-                for_x, for_y, for_z,
-                right_x, right_y, right_z,
-                up_x, up_y, up_z
-            ) = map(float, line[1:].split())
-            # The engine actually gave us a right vector, so we need to flip that.
-            left_x, left_y, left_z = -right_x, -right_y, -right_z
+    for data in rotation_data:
+        mat = Matrix.from_angle(*data.angle)
+        assert_rot(mat, Matrix.from_angle(Angle(data.angle)))
 
-            mat = Matrix.from_angle(pit, yaw, roll)
-            assert_rot(mat, Matrix.from_angle(Angle(pit, yaw, roll)))
+        # Then check rotating vectors works correctly.
+        assert_vec(X @ mat, data.for_x, data.for_y, data.for_z)
+        assert_vec(Y @ mat, data.left_x, data.left_y, data.left_z)
+        assert_vec(Z @ mat, data.up_x, data.up_y, data.up_z)
 
-            # Then check rotating vectors works correctly.
-            assert_vec(X @ mat, for_x, for_y, for_z)
-            assert_vec(Y @ mat, left_x, left_y, left_z)
-            assert_vec(Z @ mat, up_x, up_y, up_z)
+        # Check the direct matrix values.
+        assert_vec(mat.forward(), data.for_x, data.for_y, data.for_z)
+        assert_vec(mat.left(), data.left_x, data.left_y, data.left_z)
+        assert_vec(mat.up(), data.up_x, data.up_y, data.up_z)
 
-            # Check the direct matrix values.
-            assert_vec(mat.forward(), for_x, for_y, for_z)
-            assert_vec(mat.left(), left_x, left_y, left_z)
-            assert_vec(mat.up(), up_x, up_y, up_z)
+        assert math.isclose(data.for_x, mat[0, 0], abs_tol=EPSILON)
+        assert math.isclose(data.for_y, mat[0, 1], abs_tol=EPSILON)
+        assert math.isclose(data.for_z, mat[0, 2], abs_tol=EPSILON)
 
-            assert math.isclose(for_x, mat[0, 0], abs_tol=EPSILON)
-            assert math.isclose(for_y, mat[0, 1], abs_tol=EPSILON)
-            assert math.isclose(for_z, mat[0, 2], abs_tol=EPSILON)
+        assert math.isclose(data.left_x, mat[1, 0], abs_tol=EPSILON)
+        assert math.isclose(data.left_y, mat[1, 1], abs_tol=EPSILON)
+        assert math.isclose(data.left_z, mat[1, 2], abs_tol=EPSILON)
 
-            assert math.isclose(left_x, mat[1, 0], abs_tol=EPSILON)
-            assert math.isclose(left_y, mat[1, 1], abs_tol=EPSILON)
-            assert math.isclose(left_z, mat[1, 2], abs_tol=EPSILON)
+        assert math.isclose(data.up_x, mat[2, 0], abs_tol=EPSILON)
+        assert math.isclose(data.up_y, mat[2, 1], abs_tol=EPSILON)
+        assert math.isclose(data.up_z, mat[2, 2], abs_tol=EPSILON)
 
-            assert math.isclose(up_x, mat[2, 0], abs_tol=EPSILON)
-            assert math.isclose(up_y, mat[2, 1], abs_tol=EPSILON)
-            assert math.isclose(up_z, mat[2, 2], abs_tol=EPSILON)
 
-            # Also test Matrix.from_basis().
-            x = Vec(for_x, for_y, for_z)
-            y = Vec(left_x, left_y, left_z)
-            z = Vec(up_x, up_y, up_z)
-            assert_rot(Matrix.from_basis(x=x, y=y, z=z), mat)
-            assert_rot(Matrix.from_basis(x=x, y=y), mat)
-            assert_rot(Matrix.from_basis(y=y, z=z), mat)
-            assert_rot(Matrix.from_basis(x=x, z=z), mat)
+def test_from_basis_w_engine_data(py_c_vec: PyCVec, rotation_data) -> None:
+    """Test matrix.from_basis() reproduces the matrix."""
+    Vec, Angle, Matrix, parse_vec_str = py_c_vec
+    for data in rotation_data:
+        mat = Matrix.from_angle(*data.angle)
 
-            # Angle.from_basis() == Matrix.from_basis().to_angle().
-            assert_ang(Angle.from_basis(x=x, y=y, z=z), *Matrix.from_basis(x=x, y=y, z=z).to_angle())
-            assert_ang(Angle.from_basis(x=x, y=y), *Matrix.from_basis(x=x, y=y).to_angle())
-            assert_ang(Angle.from_basis(y=y, z=z), *Matrix.from_basis(y=y, z=z).to_angle())
-            assert_ang(Angle.from_basis(x=x, z=z), *Matrix.from_basis(x=x, z=z).to_angle())
+        x = Vec(data.for_x, data.for_y, data.for_z)
+        y = Vec(data.left_x, data.left_y, data.left_z)
+        z = Vec(data.up_x, data.up_y, data.up_z)
+        assert_rot(Matrix.from_basis(x=x, y=y, z=z), mat)
+        assert_rot(Matrix.from_basis(x=x, y=y), mat)
+        assert_rot(Matrix.from_basis(y=y, z=z), mat)
+        assert_rot(Matrix.from_basis(x=x, z=z), mat)
 
-            # And Vec.cross().
-            assert_vec(Vec.cross(x, y), up_x, up_y, up_z, tol=1e-5)
-            assert_vec(Vec.cross(y, z), for_x, for_y, for_z, tol=1e-5)
-            assert_vec(Vec.cross(x, z), -left_x, -left_y, -left_z, tol=1e-5)
+        # Angle.from_basis() == Matrix.from_basis().to_angle().
+        assert_ang(Angle.from_basis(x=x, y=y, z=z), *Matrix.from_basis(x=x, y=y, z=z).to_angle())
+        assert_ang(Angle.from_basis(x=x, y=y), *Matrix.from_basis(x=x, y=y).to_angle())
+        assert_ang(Angle.from_basis(y=y, z=z), *Matrix.from_basis(y=y, z=z).to_angle())
+        assert_ang(Angle.from_basis(x=x, z=z), *Matrix.from_basis(x=x, z=z).to_angle())
 
-            assert_vec(Vec.cross(y, x), -up_x, -up_y, -up_z, tol=1e-5)
-            assert_vec(Vec.cross(z, y), -for_x, -for_y, -for_z, tol=1e-5)
-            assert_vec(Vec.cross(z, x), left_x, left_y, left_z, tol=1e-5)
 
-            assert_vec(Vec.cross(x, x), 0, 0, 0)
-            assert_vec(Vec.cross(y, y), 0, 0, 0)
-            assert_vec(Vec.cross(z, z), 0, 0, 0)
+def test_vec_cross_w_engine_data(py_c_vec: PyCVec, rotation_data) -> None:
+    """Test Vec.cross() with engine rotation data."""
+    Vec, Angle, Matrix, parse_vec_str = py_c_vec
+    for data in rotation_data:
+        x = Vec(data.for_x, data.for_y, data.for_z)
+        y = Vec(data.left_x, data.left_y, data.left_z)
+        z = Vec(data.up_x, data.up_y, data.up_z)
+
+        assert_vec(Vec.cross(x, y), data.up_x, data.up_y, data.up_z, tol=1e-5)
+        assert_vec(Vec.cross(y, z), data.for_x, data.for_y, data.for_z, tol=1e-5)
+        assert_vec(Vec.cross(x, z), -data.left_x, -data.left_y, -data.left_z, tol=1e-5)
+
+        assert_vec(Vec.cross(y, x), -data.up_x, -data.up_y, -data.up_z, tol=1e-5)
+        assert_vec(Vec.cross(z, y), -data.for_x, -data.for_y, -data.for_z, tol=1e-5)
+        assert_vec(Vec.cross(z, x), data.left_x, data.left_y, data.left_z, tol=1e-5)
+
+        assert_vec(Vec.cross(x, x), 0, 0, 0)
+        assert_vec(Vec.cross(y, y), 0, 0, 0)
+        assert_vec(Vec.cross(z, z), 0, 0, 0)
 
 
 def test_copy_pickle(py_c_vec: PyCVec) -> None:
