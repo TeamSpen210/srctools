59f2e7ea69859b475be1d6029ea67af724e0c31c
Spencer Brown <spencerb21@live.com>
2022-08-25 19:23:13 +1000

    Force parameters to appear on their own line

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,6 +69,10 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = [
+    'css/custom.css',
+]
+
 
 def setup(app: Sphinx) -> None:
     """Perform modifications."""
