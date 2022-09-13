ba72aa2fe2f4083f7d90063e43cd11048633cea0
Spencer Brown <spencerb21@live.com>
2022-09-13 17:08:43 +1000

    Add more smoke tests for loading various databases.

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -14,3 +14,21 @@ package_loc = Path(__file__, '..', '..', 'src', 'srctools')
 def test_smoke(mod_name: str) -> None:
     """Ensure every module is importable."""
     importlib.import_module('srctools.' + mod_name)
+
+
+def test_entclass_resources() -> None:
+    """Test the class resources database can be loaded."""
+    from srctools.packlist import entclass_resources
+    assert list(entclass_resources('info_target')) == []
+
+
+def test_vmt_types() -> None:
+    """Test the VMT types database can be loaded."""
+    from srctools.vmt import get_parm_type, VarType
+    assert get_parm_type('$basetexture') is VarType.TEXTURE
+
+
+def test_engine_fgd() -> None:
+    """Test the FGD database can be loaded."""
+    from srctools.fgd import FGD
+    assert 'env_beam' in FGD.engine_dbase().entities
