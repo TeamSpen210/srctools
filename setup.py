af93e1df2069d2ac71db53d4bab7dab4bad384bd
Spencer Brown <spencerb21@live.com>
2022-10-02 18:56:25 +1000

    Install srctools when building docs

--- setup.py
+++ setup.py
@@ -17,50 +17,53 @@ elif MAC:
 else:
     openmp = ['-fopenmp']
 
-setup(
-    # Setuptools automatically runs Cython, if available.
-    ext_modules=[
-        Extension(
-            "srctools._tokenizer",
-            sources=["src/srctools/_tokenizer.pyx"],
-            optional=optional_ext,
-            extra_compile_args=[
-                # '/FAs',  # MS ASM dump
-            ],
-        ),
-        Extension(
-            "srctools._cy_vtf_readwrite",
-            include_dirs=[os.path.abspath(os.path.join(root, "src", "libsquish"))],
-            language='c++',
-            optional=optional_ext,
-            sources=[
-                "src/srctools/_cy_vtf_readwrite.pyx",
-                "src/libsquish/alpha.cpp",
-                "src/libsquish/clusterfit.cpp",
-                "src/libsquish/colourblock.cpp",
-                "src/libsquish/colourfit.cpp",
-                "src/libsquish/colourset.cpp",
-                "src/libsquish/maths.cpp",
-                "src/libsquish/rangefit.cpp",
-                "src/libsquish/singlecolourfit.cpp",
-                "src/libsquish/squish.cpp",
-            ],
-            extra_compile_args=[
-                # '/FAs',  # MS ASM dump
-            ] + openmp,
-            extra_link_args=openmp,
-        ),
-        Extension(
-            "srctools._math",
-            include_dirs=[os.path.abspath(os.path.join(root, "src", "quickhull/"))],
-            language='c++',
-            optional=optional_ext,
-            sources=["src/srctools/_math.pyx", "src/quickhull/QuickHull.cpp"],
-            extra_compile_args=[
-                # '/FAs',  # MS ASM dump
-            ] if WIN else [
-                '-std=c++11',  # Needed for Mac to work
-            ],
-        ),
-    ],
-)
+extensions = [
+    Extension(
+        "srctools._tokenizer",
+        sources=["src/srctools/_tokenizer.pyx"],
+        optional=optional_ext,
+        extra_compile_args=[
+            # '/FAs',  # MS ASM dump
+        ],
+    ),
+    Extension(
+        "srctools._cy_vtf_readwrite",
+        include_dirs=[os.path.abspath(os.path.join(root, "src", "libsquish"))],
+        language='c++',
+        optional=optional_ext,
+        sources=[
+            "src/srctools/_cy_vtf_readwrite.pyx",
+            "src/libsquish/alpha.cpp",
+            "src/libsquish/clusterfit.cpp",
+            "src/libsquish/colourblock.cpp",
+            "src/libsquish/colourfit.cpp",
+            "src/libsquish/colourset.cpp",
+            "src/libsquish/maths.cpp",
+            "src/libsquish/rangefit.cpp",
+            "src/libsquish/singlecolourfit.cpp",
+            "src/libsquish/squish.cpp",
+        ],
+        extra_compile_args=[
+            # '/FAs',  # MS ASM dump
+        ] + openmp,
+        extra_link_args=openmp,
+    ),
+    Extension(
+        "srctools._math",
+        include_dirs=[os.path.abspath(os.path.join(root, "src", "quickhull/"))],
+        language='c++',
+        optional=optional_ext,
+        sources=["src/srctools/_math.pyx", "src/quickhull/QuickHull.cpp"],
+        extra_compile_args=[
+            # '/FAs',  # MS ASM dump
+        ] if WIN else [
+            '-std=c++11',  # Needed for Mac to work
+        ],
+    ),
+]
+
+# Don't build extension modules for docs, they're not useful.
+if 'READTHEDOCS' in os.environ:
+    setup()
+else:
+    setup(ext_modules=extensions)
