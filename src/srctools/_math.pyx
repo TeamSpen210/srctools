4c017c0c7b86baa3d78c43b6eec173fba32f4ce5
Spencer Brown <spencerb21@live.com>
2022-12-03 19:25:41 +1000

    Do string formatting for the Cython version

--- src/srctools/_math.pyx
+++ src/srctools/_math.pyx
@@ -3,10 +3,12 @@
 from cpython.exc cimport PyErr_WarnEx
 from cpython.object cimport Py_EQ, Py_GE, Py_GT, Py_LE, Py_LT, Py_NE, PyObject, PyTypeObject
 from cpython.ref cimport Py_INCREF
+from cpython.conversion cimport PyOS_double_to_string
+from cpython.mem cimport PyMem_Malloc, PyMem_Free
 from libc cimport math
 from libc.math cimport NAN, cos, llround, sin, tan
 from libc.stdint cimport uint_fast8_t
-from libc.stdio cimport sscanf
+from libc.stdio cimport sscanf, snprintf
 from libc.string cimport memcmp, memcpy, memset
 from libcpp.vector cimport vector
 cimport cython.operator
@@ -137,6 +139,81 @@ cdef inline double norm_ang(double val):
     # We have to double-modulus because -1e-14 % 360.0 = 360.0.
     val = val % 360.0 % 360.0
     return val
+
+
+cdef int trim_float(char *buf):
+    """Strip a .0 from the end of a float."""
+    cdef int size = len(buf)
+    cdef int large = size
+    print(repr(buf[:large]), end='')
+    while size > 1 and buf[size - 1] == b'0':
+        buf[size - 1] = 0
+        size -= 1
+    if size > 1 and buf[size - 1] == b'.':
+        buf[size - 1] = 0
+        size -= 1
+    print(repr(buf[:large]))
+
+
+cdef char * _format_float(double x, int places) except NULL:
+    """Convert the specified float to a string, stripping off a .0 if it ends with that."""
+    cdef char *buf
+    buf = PyOS_double_to_string(x, b'f', places, 0, NULL)
+    trim_float(buf)
+    return buf
+
+
+cdef str _format_triple(const char *fmt, const vec_t *values):
+    """Format three floats into the specified format string."""
+    cdef size_t size1, size2
+    cdef char *xbuf = NULL
+    cdef char *ybuf = NULL
+    cdef char *zbuf = NULL
+    cdef char *buf = NULL
+    try:
+        xbuf = _format_float(values.x, 6)
+        ybuf = _format_float(values.y, 6)
+        zbuf = _format_float(values.z, 6)
+        size1 = snprintf(NULL, 0, fmt, xbuf, ybuf, zbuf)
+        buf = <char *>PyMem_Malloc(size1 + 1)
+        if buf == NULL:
+            raise MemoryError
+        size2 = snprintf(buf, size1 + 1, fmt, xbuf, ybuf, zbuf)
+        if size1 != size2:
+            raise SystemError('Could not format numbers!')
+        return buf[:size2].decode('ascii')
+    finally:
+        PyMem_Free(xbuf)
+        PyMem_Free(ybuf)
+        PyMem_Free(zbuf)
+        PyMem_Free(buf)
+
+
+cdef str _join_triple(const vec_t *values, str joiner):
+    """Format three floats, with a delimiter."""
+    cdef size_t size1, size2
+    cdef char *xbuf = NULL
+    cdef char *ybuf = NULL
+    cdef char *zbuf = NULL
+    cdef char *buf = NULL
+    cdef char *join_b = PyUnicode_AsUTF8AndSize(joiner, NULL)
+    try:
+        xbuf = _format_float(values.x, 6)
+        ybuf = _format_float(values.y, 6)
+        zbuf = _format_float(values.z, 6)
+        size1 = snprintf(NULL, 0, b'%s%s%s%s%s', xbuf, join_b, ybuf, join_b, zbuf)
+        buf = <char *>PyMem_Malloc(size1 + 1)
+        if buf == NULL:
+            raise MemoryError
+        size2 = snprintf(buf, size1 + 1, b'%s%s%s%s%s', xbuf, join_b, ybuf, join_b, zbuf)
+        if size1 != size2:
+            raise SystemError('Could not format numbers!')
+        return buf[:size2].decode('utf8')
+    finally:
+        PyMem_Free(xbuf)
+        PyMem_Free(ybuf)
+        PyMem_Free(zbuf)
+        PyMem_Free(buf)
 
 
 cdef VecBase pick_vec_type(type left, type right):
@@ -360,6 +437,16 @@ def lerp(x: float, in_min: float, in_max: float, out_min: float, out_max: float)
     If both in values are the same, ZeroDivisionError is raised.
     """
     return out_min + ((x - in_min) * (out_max - out_min)) / (in_max - in_min)
+
+
+def format_float(x: float, places: int=6) -> str:
+    """Convert the specified float to a string, stripping off a .0 if it ends with that."""
+    buf = _format_float(x, places)
+    try:
+        return buf.decode('ascii')
+    finally:
+        PyMem_Free(buf)
+
 
 
 cdef inline unsigned char conv_vec(
@@ -1522,13 +1609,12 @@ cdef class VecBase:
         )
 
 
-    def join(self, delim: str=', ') -> str:
+    def join(self, delim: str=' ') -> str:
         """Return a string with all numbers joined by the passed delimiter.
 
         This strips off the .0 if no decimal portion exists.
         """
-        # :g strips the .0 off of floats if it's an integer.
-        return f'{self.val.x:g}{delim}{self.val.y:g}{delim}{self.val.z:g}'
+        return _join_triple(&self.val, delim)
 
     def __str__(self) -> str:
         """Return the values, separated by spaces.
@@ -1536,13 +1622,39 @@ cdef class VecBase:
         This is the main format in Valve's file formats.
         This strips off the .0 if no decimal portion exists.
         """
-        return f"{self.val.x:g} {self.val.y:g} {self.val.z:g}"
+        return _format_triple(b'%s %s %s', &self.val)
 
     def __format__(self, format_spec: str) -> str:
         """Control how the text is formatted."""
+        cdef str x_str, y_str, z_str
+        cdef char *x_buf = NULL
+        cdef char *y_buf = NULL
+        cdef char *z_buf = NULL
+        cdef char *buf = NULL
+        cdef Py_ssize_t size, total
+
         if not format_spec:
-            format_spec = 'g'
-        return f"{self.val.x:{format_spec}} {self.val.y:{format_spec}} {self.val.z:{format_spec}}"
+            return _format_triple(b'%s %s %s', &self.val)
+        total = 4
+
+        x_str = format(self.val.x, format_spec)
+        x_buf = PyUnicode_AsUTF8AndSize(x_str, &size)
+        total += size
+
+        y_str = format(self.val.x, format_spec)
+        y_buf = PyUnicode_AsUTF8AndSize(y_str, &size)
+        total += size
+
+        z_str = format(self.val.x, format_spec)
+        z_buf = PyUnicode_AsUTF8AndSize(z_str, &size)
+        total += size
+
+        buf = <char *>PyMem_Malloc(total)
+        try:
+            snprintf(buf, total, b'%s %s %s', x_buf, y_buf, z_buf)
+            return buf[:total].decode('utf8')
+        finally:
+            PyMem_Free(buf)
 
     def __iter__(self) -> VecIter:
         cdef VecIter viter = VecIter.__new__(VecIter)
@@ -1632,7 +1744,7 @@ cdef class FrozenVec(VecBase):
 
     def __repr__(self) -> str:
         """Code required to reproduce this vector."""
-        return f"FrozenVec({self.val.x:g}, {self.val.y:g}, {self.val.z:g})"
+        return _format_triple(b'FrozenVec(%s, %s, %s)', &self.val)
 
     def __round__(self, object n=0):
         """Performing round() on a FrozenVec rounds each axis."""
@@ -1744,7 +1856,7 @@ cdef class Vec(VecBase):
 
     def __repr__(self) -> str:
         """Code required to reproduce this vector."""
-        return f"Vec({self.val.x:g}, {self.val.y:g}, {self.val.z:g})"
+        return _format_triple(b'Vec(%s, %s, %s)', &self.val)
 
     def __round__(self, object n=0):
         """Performing round() on a Vec rounds each axis."""
@@ -2616,9 +2728,35 @@ cdef class AngleBase:
 
     def __format__(self, format_spec: str) -> str:
         """Control how the text is formatted."""
+        cdef str x_str, y_str, z_str
+        cdef char *x_buf = NULL
+        cdef char *y_buf = NULL
+        cdef char *z_buf = NULL
+        cdef char *buf = NULL
+        cdef Py_ssize_t size, total
+
         if not format_spec:
-            format_spec = 'g'
-        return f"{self.val.x:{format_spec}} {self.val.y:{format_spec}} {self.val.z:{format_spec}}"
+            return _format_triple(b'%s %s %s', &self.val)
+        total = 4
+
+        x_str = format(self.val.x, format_spec)
+        x_buf = PyUnicode_AsUTF8AndSize(x_str, &size)
+        total += size
+
+        y_str = format(self.val.x, format_spec)
+        y_buf = PyUnicode_AsUTF8AndSize(y_str, &size)
+        total += size
+
+        z_str = format(self.val.x, format_spec)
+        z_buf = PyUnicode_AsUTF8AndSize(z_str, &size)
+        total += size
+
+        buf = <char *>PyMem_Malloc(total)
+        try:
+            snprintf(buf, total, b'%s %s %s', x_buf, y_buf, z_buf)
+            return buf[:total].decode('utf8')
+        finally:
+            PyMem_Free(buf)
 
     def as_tuple(self):
         """Return the Angle as a tuple."""
@@ -2821,7 +2959,7 @@ cdef class FrozenAngle(AngleBase):
         return _angle_mut(self.val.x, self.val.y, self.val.z)
 
     def __repr__(self) -> str:
-        return f'FrozenAngle({self.val.x:g}, {self.val.y:g}, {self.val.z:g})'
+        return _format_triple(b'FrozenAngle(%s, %s, %s)', &self.val)
 
     def __copy__(self):
         """FrozenAngle is immutable."""
@@ -2904,7 +3042,7 @@ cdef class Angle(AngleBase):
         self.val.z = norm_ang(roll)
 
     def __repr__(self) -> str:
-        return f'Angle({self.val.x:g}, {self.val.y:g}, {self.val.z:g})'
+        return _format_triple(b'Angle(%s, %s, %s)', &self.val)
 
     @classmethod
     def from_basis(
