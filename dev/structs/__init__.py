ba2f26f923d06d12849574c0faad8465040d9945
Spencer Brown <spencerb21@live.com>
2022-12-21 10:23:08 +1000

    Add some code drawing the layout of the static prop lump, for analysis purposes

---
+++ dev/structs/__init__.py
@@ -0,0 +1,118 @@
+"""Analyse structs."""
+from functools import partial
+from typing import Iterator, Literal
+
+import attrs
+
+
+INDENT = ''
+
+class Member:
+    """A member in a struct."""
+    def __len__(self) -> int:
+        """Fixed in bytes."""
+        raise NotImplementedError
+
+    def display(self, indent: str, name: str) -> Iterator[str]:
+        raise NotImplementedError
+
+
+@attrs.frozen
+class Fixed(Member):
+    """A fixed size member."""
+    size: int
+    kind: Literal['s', 'u', 'f', 'pad', 'unk', 'str']
+    def __len__(self):
+        return self.size
+
+    def display(self, indent: str, name: str) -> Iterator[str]:
+        if self.kind == 'pad':
+            yield from ['0'] * self.size
+        elif self.kind == 'unk':
+            yield from ['?'] * self.size
+        else:
+            if self.kind == 'str':
+                yield f'{indent}str[{self.size}] {name}'
+            else:
+                yield f'{indent}{self.kind}{self.size*8} {name}'
+            for _ in range(1, self.size):
+                yield ' ' * len(indent) + '.'
+
+
+@attrs.frozen(init=False)
+class Struct(Member):
+    """A structure."""
+    contents: dict[str, Member]
+    def __init__(self, **members: Member) -> None:
+        self.__attrs_init__(members)
+
+    def __len__(self) -> int:
+        """A struct is the size of the contents."""
+        return sum(map(len, self.contents.values()))
+
+    def display(self, indent: str, name: str) -> Iterator[str]:
+        for attr_name, attr in self.contents.items():
+            if attr_name == 'base':
+                yield from attr.display(indent, name)
+            elif attr_name.startswith('_'):
+                yield from [indent + '-'] * len(attr)   # Padding etc.
+            else:
+                yield from attr.display(indent + INDENT, f'{name}.{attr_name}' if name else attr_name)
+
+
+type_uchar = Fixed(1, 'u')
+type_char = Fixed(1, 's')
+type_ushort = Fixed(2, 'u')
+type_short = Fixed(2, 's')
+type_uint = Fixed(4, 'u')
+type_int = Fixed(4, 's')
+type_ulong = Fixed(8, 'u')
+type_long = Fixed(8, 's')
+
+type_u8 = Fixed(1, 'u')
+type_s8 = Fixed(1, 's')
+type_u16 = Fixed(2, 'u')
+type_s16 = Fixed(2, 's')
+type_u32 = Fixed(4, 'u')
+type_s32 = Fixed(4, 's')
+type_u64 = Fixed(8, 'u')
+type_s64 = Fixed(8, 's')
+
+type_float = Fixed(4, 'f')
+type_double = Fixed(8, 'f')
+
+type_vector = Struct(x=type_float, y=type_float, z=type_float)
+type_angles = Struct(pitch=type_float, yaw=type_float, roll=type_float)
+type_color32 = Struct(r=type_u8, g=type_u8, b=type_u8, a=type_u8)
+
+
+def display_all(ns: dict[str, object]) -> None:
+    structs = []
+    columns = []
+    for name, value in ns.items():
+        if name.startswith('prop_') and isinstance(value, Member):
+            data = [f' {name[5:]} ({len(value)})']
+            data.extend(value.display('', ''))
+            print(data[0])
+            columns.append(max(map(len, data)))
+            data.insert(1, '=' * columns[-1])
+            structs.append(data)
+
+    print()
+
+    iters = [(iter(lst), size) for lst, size in zip(structs, columns)]
+    remaining = True
+    while remaining:
+        remaining = False
+        print('| ', end='')
+        for it, size in iters:
+            try:
+                cell = next(it)
+                remaining = True
+            except StopIteration:
+                cell = '#' * size
+            if cell in ('?', '0'):
+                print('', cell * (size - 2), '', end=' | ')
+            else:
+                print(f'{cell: <{size}}', end=' | ')
+        print()
---
+++ dev/structs/static_prop.py
@@ -0,0 +1,93 @@
+from structs import *
+
+prop_v4 = Struct(
+    origin=type_vector,
+    angles=type_angles,
+    proptype=type_u16,
+    firstLeaf=type_u16,
+    leafCount=type_u16,
+    solid=type_u8,
+    flags=type_u8,
+    skin=type_int,
+    fademin=type_float,
+    fademax=type_float,
+    lighting=type_vector,
+)
+
+prop_v5 = Struct(base=prop_v4, forcedFadeScale=type_float)
+prop_v5_ship = Struct(base=prop_v5, targetname=Fixed(128, 'str'))
+
+prop_v6 = Struct(base=prop_v5, minDX=type_ushort, maxDX=type_ushort)
+prop_v6_bloody = Struct(base=prop_v6, targetname=Fixed(128, 'str'))
+
+prop_v7_l4d = Struct(base=prop_v6, rendercolor=type_color32)
+
+prop_v7_zeno_clash = Struct(base=prop_v6, unknown=type_u32)
+
+prop_v8 = Struct(
+    base=prop_v5,
+    minCPU=type_u8,
+    maxCPU=type_u8,
+    minGPU=type_u8,
+    maxGPU=type_u8,
+    rendercolor=type_color32,
+)
+
+prop_v9 = Struct(
+    base=prop_v8,
+    disableX360=type_u8,
+    padding=Fixed(3, 'pad'),
+)
+
+# Not really relevant?
+# prop_v9_d_est = Struct(
+#     origin=type_vector,
+#     angles=type_angles,
+#     proptype=type_u16,
+#     firstLeaf=type_u16,
+#     leafCount=type_u16,
+#     solid=type_u8,
+#     flags=type_u8,
+#     _1=Fixed(4, 'unk'),
+#     skin=type_int,
+#     fademin=type_float,
+#     fademax=type_float,
+#     _2=Fixed(12, 'unk'),
+#     forcedFade=type_float,
+#     minCPU=type_u8,
+#     maxCPU=type_u8,
+#     minGPU=type_u8,
+#     maxGPU=type_u8,
+#     _3=type_u8,
+#     rendercolor=type_color32,
+#     _4=Fixed(3, 'unk'),
+# )
+
+prop_v10 = Struct(
+    base=prop_v6,
+    flags2=type_uint,
+    lightmap=Struct(x=type_ushort, y=type_ushort),
+)
+
+prop_v10_csgo = Struct(
+    base=prop_v9,
+    # unknown=Fixed(4, 'unk')
+    flags2=type_uint,
+)
+
+prop_v11_lite = Struct(
+    base=prop_v10,
+    rendercolor=type_color32,
+)
+
+prop_v11 = Struct(
+    base=prop_v11_lite,
+    flags2=type_uint,
+)
+
+prop_v11_csgo = Struct(
+    base=prop_v10_csgo,
+    scale=type_float,
+)
+
+display_all(globals())
