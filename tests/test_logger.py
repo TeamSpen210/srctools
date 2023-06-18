931e0f47fb5e0d5aad456e8e5b148016ec4a2a6f
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2023-06-19 00:25:21 +0200

    Remove spaces to comply with ruff

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -7,7 +7,7 @@ def test_logging_output() -> None:
     """Test the output of logging to the console."""
     from srctools.logger import init_logging, get_logger
     old, sys.stdout = sys.stdout, StringIO()
-    init_logging().info( 'hello there' )
+    init_logging().info('hello there')
     output, sys.stdout = sys.stdout, old
     output.seek(0)
     assert output.read() == '[I] test_logger.test_logging_output(): hello there\n'
