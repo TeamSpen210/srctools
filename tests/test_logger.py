53ee17886ba001ab181e923053d72d6843e429ea
Spencer Brown <spencerb21@live.com>
2023-06-21 12:19:33 +1000

    Switch logger test to be a regression test

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,13 +1,35 @@
-"""Import all modules, to ensure at least imports work."""
+"""Test the logging system."""
+from logging import Logger, getLogger as stdlib_getlogger
 import sys
-from io import StringIO
 
 
-def test_logging_output() -> None:
+def function(logger: Logger) -> None:
+    """Test detecting different methods."""
+    logger.info('Starting other function')
+    logger.warning('Used wrong logic')
+    logger.info('Finishing.')
+
+
+def test_logging_output(capsys, file_regression, monkeypatch) -> None:
     """Test the output of logging to the console."""
-    from srctools.logger import init_logging, get_logger
-    old, sys.stdout = sys.stdout, StringIO()
-    init_logging().info('hello there')
-    output, sys.stdout = sys.stdout, old
-    output.seek(0)
-    assert output.read() == '[I] test_logger.test_logging_output(): hello there\n'
+    from srctools.logger import init_logging, get_logger, context
+
+    # Init_logging modifies sys.excepthook, so ensure we undo that.
+    monkeypatch.setattr(sys, 'excepthook', sys.excepthook)
+    monkeypatch.setattr(stdlib_getlogger(), 'handlers', [])
+
+    root = init_logging()
+    root.info('hello there')
+    root.error('Root error!:\n- Something failed.')
+    get_logger('another').warning('A problem: {}', 45)
+    function(root)
+    with context('First'):
+        root.info('Message')
+        with context('Second'):
+            root.info('More messages')
+        root.warning('A warning.')
+
+    out, err = capsys.readouterr()
+    # Merge both files, so if the regression fails the result of both parts is visible.
+    combined = f'{out}\n<<<<< STDOUT | STDERR >>>>>\n{err}'
+    file_regression.check(combined, encoding='utf8')
