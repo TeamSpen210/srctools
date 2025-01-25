a4e56fcf987eb08495efcd8ada323cad1c9ff1e1
Spencer Brown <spencerb21@live.com>
2025-01-25 17:59:32 +1000

    Rewrite to store directly as a list of verts, instead of half-edges

--- src/srctools/geometry.py
+++ src/srctools/geometry.py
@@ -1,133 +1,85 @@
 """Implements tools for manipulating geometry."""
+from typing import Optional
+
 import collections
 import math
-from typing import Optional, Iterator, List
 
-import attr
-from srctools import Vec
-from srctools.smd import Vertex
+import attrs
+
+from srctools import FrozenVec, Vec
 from srctools import vmf
+from srctools.bsp import Plane
 
 
-@attr.define(eq=False)
-class Face:
-    """A face."""
+@attrs.define(eq=False)
+class Polygon:
+    """A face, including the associated vertices."""
     original: Optional[vmf.Side]
-    first_edge: 'HalfEdge'
-    norm: Vec
-    dist: float
-
-
-@attr.define(eq=False)
-class HalfEdge:
-    """A half-edge data structure."""
-    end_pos: Vertex
-    face: Optional[Face]  # Attached face.
-    pair: 'HalfEdge'  # The opposite edge. ``self.pair.pair is self``
-    next: 'HalfEdge'  # Edge in front. ``self.next.pair.end_pos is self.end_pos``
-    prev: 'HalfEdge'  # Edge behind us. ``self.prev.end_pos is self.pair.end_pos``
+    vertices: list[FrozenVec]
+    plane: Plane
 
     @property
-    def start_pos(self) -> Vertex:
-        """Our pair's position, which is the 'start' position for us."""
-        return self.pair.end_pos
+    def norm(self) -> Vec:
+        return self.plane.normal
 
     @property
-    def length(self) -> float:
-        return (self.pair.end_pos.pos - self.end_pos.pos).mag()
-
-    def iter_around_face(self) -> Iterator['HalfEdge']:
-        """Move through the edges neighbouring our face."""
-        yield self
-        edge = self.next
-        while edge is not self:
-            yield edge
-            edge = edge.next
-
-    def iter_around_vert(self) -> Iterator['HalfEdge']:
-        """Move through the edges neighbouring our end vertex."""
-        yield self
-        # To move around a vertex, we move to the next one, then flip
-        # so we reversed and point back at the vert.
-        edge = self.next.pair
-        while edge is not self:
-            yield edge
-            edge = edge.next.pair
+    def plane_dist(self) -> float:
+        return self.plane.dist
 
 
-def from_brush(brush: vmf.Solid) -> List[Face]:
-    """Convert a VMF brush into a set of edges."""
-    faces = {}
+def from_brush(brush: vmf.Solid) -> list[Polygon]:
+    """Convert a VMF brush into a set of polygons with vertices computed."""
+    polys = {}
     for side in brush:
         norm = side.normal()
-        faces[side] = Face(side, None, norm, Vec.dot(side.planes[0], norm))
+        polys[side] = Polygon(side, [], Plane(norm, Vec.dot(side.planes[0], norm)))
 
-    all_verts: dict[tuple[float, float, float], Vertex] = {}
-    edges: dict[tuple[Vertex, Vertex], HalfEdge] = {}
+    all_verts: dict[FrozenVec, FrozenVec] = {}
 
     # First compute all the vertices.
-    # Face verts will then hold a vert and the other two planes.
-    face_list = list(faces.values())
-    face_verts: dict[Face, set[Vertex]] = {face: set() for face in face_list}
-    vert_faces: dict[Vertex, set[Face]] = collections.defaultdict(set)
-    for i, face_a in enumerate(face_list):
-        for j, face_b in enumerate(face_list[i:], i):
-            for face_c in face_list[j:]:
-                divisor = Vec.dot(Vec.cross(face_a.norm, face_b.norm), face_c.norm)
+    # Poly verts will then hold a vert and the other two planes.
+    poly_list = list(polys.values())
+    poly_verts: dict[Polygon, set[FrozenVec]] = {poly: set() for poly in poly_list}
+    vert_polys: dict[FrozenVec, set[Polygon]] = collections.defaultdict(set)
+    for i, poly_a in enumerate(poly_list):
+        for j, poly_b in enumerate(poly_list[i:], i):
+            for poly_c in poly_list[j:]:
+                divisor = Vec.dot(Vec.cross(poly_a.norm, poly_b.norm), poly_c.norm)
                 if abs(divisor) < 0.001:
                     continue
 
-                vert_pos = (
-                    face_a.dist * Vec.cross(face_b.norm, face_c.norm) +
-                    face_b.dist * Vec.cross(face_c.norm, face_a.norm) +
-                    face_c.dist * Vec.cross(face_a.norm, face_b.norm)
+                vert = (
+                    poly_a.plane_dist * FrozenVec.cross(poly_b.norm, poly_c.norm) +
+                    poly_b.plane_dist * FrozenVec.cross(poly_c.norm, poly_a.norm) +
+                    poly_c.plane_dist * FrozenVec.cross(poly_a.norm, poly_b.norm)
                 ) / divisor
-                try:
-                    vert = all_verts[vert_pos.as_tuple()]
-                except KeyError:
-                    vert = all_verts[vert_pos.as_tuple()] = Vertex(vert_pos, Vec(), 0.0, 0.0, [])
+                vert = all_verts.setdefault(vert, vert)
                 # We need to now also check it's inside the brush, since three
                 # planes can potentially also intersect outside the brush.
-                for face in face_list:
-                    if Vec.dot(vert.pos, face.norm) - face.dist < -0.01:
+                for poly in poly_list:
+                    if Vec.dot(vert, poly.norm) - poly.plane_dist < -0.01:
                         break
                 else:
-                    face_verts[face_a].add(vert)
-                    face_verts[face_b].add(vert)
-                    face_verts[face_c].add(vert)
-                    vert_faces[vert] |= {face_a, face_b, face_c}
+                    poly_verts[poly_a].add(vert)
+                    poly_verts[poly_b].add(vert)
+                    poly_verts[poly_c].add(vert)
+                    vert_polys[vert] |= {poly_a, poly_b, poly_c}
 
-    face_centers = {
-        face: sum((vert.pos for vert in verts), Vec()) / len(verts)
-        for face, verts in face_verts.items()
+    poly_centers = {
+        poly: sum((vert for vert in verts), FrozenVec()) / len(verts)
+        for poly, verts in poly_verts.items()
     }
 
-    for face, vert_set in face_verts.items():
+    for poly, vert_set in poly_verts.items():
         # Create a pair of basis vectors orthogonal to the normal,
         # then we can use those to sort the vectors by their angle around
         # the normal - putting them in order. Now we have an edge loop.
-        vert_list = list(vert_set)
-        u = (vert_list[1].pos - vert_list[0].pos).norm()
-        v = Vec.cross(u, face.norm)
-        cent = face_centers[face]
-        vert_list.sort(key=lambda vt: math.atan2(
+        poly.vertices = list(vert_set)
+        u = (poly.vertices[1] - poly.vertices[0]).norm()
+        v = FrozenVec.cross(u, poly.norm)
+        cent = poly_centers[poly]
+        poly.vertices.sort(key=lambda vt: math.atan2(
             Vec.dot(v, vt.pos - cent),
             Vec.dot(u, vt.pos - cent),
         ))
-        last_vert = vert_list[-1]
-        edge_loop: list[HalfEdge] = []
-
-        for i, vert in enumerate(vert_list):
-            edge = edges[last_vert, vert] = HalfEdge(vert, face, None, None, None)
-            edge_loop.append(edge)
-            try:
-                edge.pair = edges[vert, last_vert]
-                edge.pair.pair = edge
-            except KeyError:  # Not constructed yet.
-                pass
-            last_vert = vert
-        for i, edge in enumerate(edge_loop):
-            edge.next = edge_loop[(i + 1) % len(edge_loop)]
-            edge.prev = edge_loop[(i - 1) % len(edge_loop)]
-        face.first_edge = edge_loop[0]
-    return face_list
+    return poly_list
